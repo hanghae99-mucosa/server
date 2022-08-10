63f691ca30326a0f8a53b8f4cc3b1291cf138ea2
jjucc99 <59110017+jjucc99@users.noreply.github.com>
2022-08-10 20:26:31 +0900

    Feat: Create ProductRequsetDto & ResponseDto!

---
+++ src/main/java/com/hanghae99/mocosa/layer/dto/product/ProductResponseDto.java
@@ -0,0 +1,37 @@
+package com.hanghae99.mocosa.layer.dto.product;
+
+import lombok.Getter;
+
+@Getter
+public class ProductResponseDto {
+    private final Long product_id;
+    private final String name;
+    private final String thumbnail;
+    private final String brandName;
+    private final String category;
+    private final int price;
+    private final int amount;
+    private final int reviewNum;
+    private final float reviewAvg;
+
+    public ProductResponseDto(Long product_id,
+                              String name,
+                              String thumbnail,
+                              String brandName,
+                              String category,
+                              int price,
+                              int amount,
+                              int reviewNum,
+                              float reviewAvg
+                              ) {
+        this.product_id = product_id;
+        this.name = name;
+        this.thumbnail = thumbnail;
+        this.brandName = brandName;
+        this.category = category;
+        this.price = price;
+        this.amount = amount;
+        this.reviewNum = reviewNum;
+        this.reviewAvg = reviewAvg;
+    }
+}

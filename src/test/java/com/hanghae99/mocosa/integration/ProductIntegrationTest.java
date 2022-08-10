3feacbdc873ef655e9b299ce65635bd6cbc7fccf
dongil618 <rlaehddlf618@gmail.com>
2022-08-10 16:17:56 +0900

    test : 검색 기능에 대한 IntegrationTest 코드 작성

--- src/test/java/com/hanghae99/mocosa/integration/ProductIntegrationTest.java
+++ src/test/java/com/hanghae99/mocosa/integration/ProductIntegrationTest.java
@@ -3,6 +3,7 @@ package com.hanghae99.mocosa.integration;
 import com.hanghae99.mocosa.config.exception.ErrorResponseDto;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -20,61 +23,64 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @ActiveProfiles("test")
 public class ProductIntegrationTest {
 
+    static final double TOLERANCE = 0.001;
+
     @Autowired
     private TestRestTemplate restTemplate;
+
 
     @Test
     @DisplayName("검색에 성공한 케이스 - 필터 제외")
     public void case1(){
         //given
-        String keyword = "무탠다드";
+        String keyword = "티셔츠";
 
         //when
-        ResponseEntity<SearchResponseDto> response = restTemplate
+        ResponseEntity<SearchResponseDtoList> response = restTemplate
                 .getForEntity(
-                        "/api/search?keyword="+keyword,
-                        SearchResponseDto.class
+                        "/api/search?keyword=" + keyword,
+                        SearchResponseDtoList.class
                 );
 
         //then
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        SearchResponseDto responseBody = response.getBody();
+        List<SearchResponseDto> responseBody = response.getBody().content;
         assertNotNull(responseBody);
 
         assertEquals(
                 1L
-                ,responseBody.getProductId());
+                ,responseBody.get(0).productId);
         assertEquals(
                 "릴렉스 핏 크루 넥 반팔 티셔츠"
-                ,responseBody.getName());
+                ,responseBody.get(0).name);
         assertEquals(
                 "image.png"
-                ,responseBody.getThumbnail());
+                ,responseBody.get(0).thumbnail);
         assertEquals(
                 "무신사 스탠다드"
-                ,responseBody.getBrandName());
+                ,responseBody.get(0).brandName);
         assertEquals(
                 "상의"
-                ,responseBody.getCategory());
+                ,responseBody.get(0).category);
         assertEquals(
                 10690
-                ,responseBody.getPrice());
+                ,responseBody.get(0).price);
         assertEquals(
                 100
-                ,responseBody.getAmount());
+                ,responseBody.get(0).amount);
         assertEquals(
                 69058
-                ,responseBody.getReviewNum());
+                ,responseBody.get(0).reviewNum);
         assertEquals(
                 4.8
-                ,responseBody.getReviewAvg());
+                ,responseBody.get(0).reviewAvg, TOLERANCE);
     }
 
     @Test
     @DisplayName("검색에 성공한 케이스 - 필터 존재")
     public void case2(){
         //given
-        String keyword = "무탠다드";
+        String keyword = "티셔츠";
         int page = 0;
         String sort = "리뷰순";
         String categoryFilter = "상의";
@@ -83,59 +89,59 @@ public class ProductIntegrationTest {
         float reviewFilter = 4.5F;
 
         //when
-        ResponseEntity<SearchResponseDto> response = restTemplate
+        ResponseEntity<SearchResponseDtoList> response = restTemplate
                 .getForEntity(
                         "/api/search?page=" + page + "&sort=" + sort + "&categoryFilter=" + categoryFilter
                                 + "&minPriceFilter=" + minPriceFilter + "&maxPriceFilter=" + maxPriceFilter
                                 + "&reviewFilter=" + reviewFilter + "&keyword=" + keyword,
-                        SearchResponseDto.class
+                        SearchResponseDtoList.class
                 );
 
         //then
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        SearchResponseDto responseBody = response.getBody();
+        List<SearchResponseDto> responseBody = response.getBody().content;
         assertNotNull(responseBody);
 
         assertEquals(
                 1L
-                ,responseBody.getProductId());
+                ,responseBody.get(0).productId);
         assertEquals(
                 "릴렉스 핏 크루 넥 반팔 티셔츠"
-                ,responseBody.getName());
+                ,responseBody.get(0).name);
         assertEquals(
                 "image.png"
-                ,responseBody.getThumbnail());
+                ,responseBody.get(0).thumbnail);
         assertEquals(
                 "무신사 스탠다드"
-                ,responseBody.getBrandName());
+                ,responseBody.get(0).brandName);
         assertEquals(
                 "상의"
-                ,responseBody.getCategory());
+                ,responseBody.get(0).category);
         assertEquals(
                 10690
-                ,responseBody.getPrice());
+                ,responseBody.get(0).price);
         assertEquals(
                 100
-                ,responseBody.getAmount());
+                ,responseBody.get(0).amount);
         assertEquals(
                 69058
-                ,responseBody.getReviewNum());
+                ,responseBody.get(0).reviewNum);
         assertEquals(
                 4.8
-                ,responseBody.getReviewAvg());
+                ,responseBody.get(0).reviewAvg, TOLERANCE);
     }
 
     @Test
     @DisplayName("검색 결과가 존재하지 않는 케이스")
     public void case3(){
         //given
-        String keyword = "무탠다드";
+        String keyword = "무탠다드 에디션";
         int page = 0;
         String sort = "리뷰순";
         String categoryFilter = "상의";
         int minPriceFilter = 10000;
         int maxPriceFilter = 50000;
-        float reviewFilter = 4.5F;
+        float reviewFilter = 5F;
 
         //when
         ResponseEntity<ErrorResponseDto> response = restTemplate
@@ -182,7 +188,7 @@ public class ProductIntegrationTest {
 
         assert responseBody != null;
         assertEquals(HttpStatus.BAD_REQUEST.value(), responseBody.getStatus());
-        assertEquals("키워드를 입력해주세요.", responseBody.getMessage());
+        assertEquals("키워드를 입력해주세요", responseBody.getMessage());
     }
 
     @Test
@@ -190,7 +196,7 @@ public class ProductIntegrationTest {
     public void case5(){
         //given
         String keyword = "무탠다드";
-        int page = -1;
+        int page = 10000;  //2147483647
         String sort = "리뷰순";
         String categoryFilter = "상의";
         int minPriceFilter = 10000;
@@ -215,6 +221,11 @@ public class ProductIntegrationTest {
         assertEquals("잘못된 접근입니다.", responseBody.getMessage());
     }
 
+    @Getter
+    @ToString
+    static class SearchResponseDtoList {
+        private List<SearchResponseDto> content;
+    }
 
     @Getter
     @Builder

fa949250c40e7dd1d4ff4c773a156f312d64f447
jjucc99 <59110017+jjucc99@users.noreply.github.com>
2022-08-12 17:59:57 +0900

    Chore: TimeStamped 기능 개선

---
+++ src/main/java/com/hanghae99/mocosa/layer/dto/order/OrderRequestDto.java
@@ -0,0 +1,12 @@
+package com.hanghae99.mocosa.layer.dto.order;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderRequestDto {
+    private int orderAmount;
+}

c45164e69812756801eee7e0554e8fa49dc22a0a
Seungchan Back <bsc980504@outlook.com>
2022-08-15 15:01:23 +0900

    Feat : Create exceptoin for All of Etc Error

--- src/main/java/com/hanghae99/mocosa/config/exception/ErrorResponseDto.java
+++ src/main/java/com/hanghae99/mocosa/config/exception/ErrorResponseDto.java
@@ -12,7 +12,6 @@ import java.time.LocalDateTime;
 @Builder
 public class ErrorResponseDto {
     private final LocalDateTime timestamp = LocalDateTime.now();
-    private final int status;
     private final String error;
     private final String code;
     private final String message;
@@ -22,7 +21,6 @@ public class ErrorResponseDto {
         return ResponseEntity
                 .status(errorCode.getHttpStatus())
                 .body(ErrorResponseDto.builder()
-                        .status(errorCode.getHttpStatus().value())
                         .error(errorCode.getHttpStatus().name())
                         .code(errorCode.name())
                         .message(errorCode.getErrorMessage())

c8493f86828f03e4517ad971a67cc392dfe30504
백승찬 <chan@baegseungchan-ui-MacBookAir.local>
2022-08-16 22:12:16 +0900

    Chore : Fix config of Jwt for Front

--- src/main/java/com/hanghae99/mocosa/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/hanghae99/mocosa/config/jwt/JwtAuthenticationFilter.java
@@ -52,6 +52,9 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 .withClaim("email", userDetails.getUsername())
                 .sign(Algorithm.HMAC512(JwtProperties.SECRET));
 
+        response.addHeader(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX + jwtToken);
+
+        // 헤더에서 잘 받을수 있어서 굳이 이렇게 할 필요가 있을까?
         JSONObject responseJson = new JSONObject();
         response.setContentType("application/json;charset=UTF-8");
         responseJson.put("token", JwtProperties.TOKEN_PREFIX + jwtToken);

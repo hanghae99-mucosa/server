d4f10fae236137c32af344ba7e36055c0ae72f8a
jjucc99 <59110017+jjucc99@users.noreply.github.com>
2022-08-18 23:24:25 +0900

    Chore: RestockNotification 기능 버그 수정 #33
    Chore: RestockNotification 기능 버그 수정 #33
    
    동일한 유저가 각각 다른 상품을 취소 했을 경우 알람을 찾을 수 없던 버그를 수정

--- src/main/java/com/hanghae99/mocosa/layer/repository/RestockNotificationRepository.java
+++ src/main/java/com/hanghae99/mocosa/layer/repository/RestockNotificationRepository.java
@@ -1,6 +1,8 @@
 package com.hanghae99.mocosa.layer.repository;
 
+import com.hanghae99.mocosa.layer.model.Product;
 import com.hanghae99.mocosa.layer.model.RestockNotification;
+import com.hanghae99.mocosa.layer.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,8 +11,11 @@ import java.util.Optional;
 
 public interface RestockNotificationRepository extends JpaRepository<RestockNotification, Long> {
 
-    @Query("select rn from RestockNotification rn join fetch rn.user u where u.userId = :userId")
-    Optional<RestockNotification> findRestockNotificationByUser_UserId(@Param("userId") Long userId);
-    @Query("select rn from RestockNotification rn join fetch rn.product p where p.productId = :productId")
-    Optional<RestockNotification> findRestockNotificationByProduct_ProductId(@Param("productId")Long productId);
+//    @Query("select rn from RestockNotification rn join fetch rn.user u where u.userId = :userId")
+//    Optional<RestockNotification> findRestockNotificationByUser_UserId(@Param("userId") Long userId);
+//    @Query("select rn from RestockNotification rn join fetch rn.product p where p.productId = :productId")
+//    Optional<RestockNotification> findRestockNotificationByProduct_ProductId(@Param("productId")Long productId);
+//    Optional<RestockNotification> findRestockNotificationByUser_UserIdAndProduct_ProductId(Long userId, Long productId);
+    Optional<RestockNotification> findRestockNotificationByUserAndProduct(User user, Product product);
+
 }

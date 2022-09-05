e5c9f45e6aa5a5fc8b3f7d7a269c98eca0ffc55e
ParkSoyun <soyp@naver.com>
2022-09-05 10:31:44 +0900

    Refactor: ProductRepositoryImpl.java의 findBySearchRequestDto() 리팩토링 (#50)

--- src/main/java/com/hanghae99/mocosa/layer/repository/ProductRepositoryImpl.java
+++ src/main/java/com/hanghae99/mocosa/layer/repository/ProductRepositoryImpl.java
@@ -10,6 +10,7 @@ import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import net.bytebuddy.description.method.ParameterList;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -28,113 +29,120 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
 
     @Override
     public Page<SearchResponseDto> findBySearchRequestDto(SearchRequestDto searchRequestDto, Pageable pageable) {
-        JPQLQuery<Product> countQuery = null;
-        List<Long> ids = null;
+        List<Long> productIdList = new ArrayList<>();
+        long totalCount = 0;
 
-        if(searchRequestDto.getKeyword().equals("ALL")) {
-            countQuery = queryFactory.selectFrom(product)
-                    .innerJoin(parentCategory).on(product.category.parentCategory.eq(parentCategory.categoryId))
-                    .where(
-                            categoryEq(searchRequestDto.getCategoryFilter()),
-                            priceBetween(searchRequestDto.getMinPriceFilter(),searchRequestDto.getMaxPriceFilter()),
-                            reviewAvgGt(searchRequestDto.getReviewFilter())
-                    );
-
-            ids = queryFactory.select(product.productId)
-                    .from(product)
-                    .innerJoin(parentCategory).on(product.category.parentCategory.eq(parentCategory.categoryId))
-                    .where(
-                            categoryEq(searchRequestDto.getCategoryFilter()),
-                            priceBetween(searchRequestDto.getMinPriceFilter(),searchRequestDto.getMaxPriceFilter()),
-                            reviewAvgGt(searchRequestDto.getReviewFilter())
-                    )
-                    .orderBy(orderBySort(searchRequestDto.getSort()))
-                    .offset(pageable.getOffset())
-                    .limit(pageable.getPageSize())
-                    .fetch();
+        if(searchRequestDto.getSearchType().equals("상품명")) {
+            productIdList = getProductIdList_productName(searchRequestDto, pageable);
+            totalCount = getTotalCount_productName(searchRequestDto);
         }
 
-        if(!searchRequestDto.getKeyword().equals("ALL") && searchRequestDto.getSearchType().equals("상품명")) {
-            countQuery = queryFactory.selectFrom(product)
-                    .innerJoin(parentCategory).on(product.category.parentCategory.eq(parentCategory.categoryId))
-                    .where(
-                            product.name.contains(searchRequestDto.getKeyword()),
-                            categoryEq(searchRequestDto.getCategoryFilter()),
-                            priceBetween(searchRequestDto.getMinPriceFilter(),searchRequestDto.getMaxPriceFilter()),
-                            reviewAvgGt(searchRequestDto.getReviewFilter())
-                    );
+        if(searchRequestDto.getSearchType().equals("브랜드명")) {
+            List<Long> brandIdList = getBrandIdList(searchRequestDto);
 
-            ids = queryFactory.select(product.productId)
-                    .from(product)
-                    .innerJoin(parentCategory).on(product.category.parentCategory.eq(parentCategory.categoryId))
-                    .where(
-                            product.name.contains(searchRequestDto.getKeyword()),
-                            categoryEq(searchRequestDto.getCategoryFilter()),
-                            priceBetween(searchRequestDto.getMinPriceFilter(),searchRequestDto.getMaxPriceFilter()),
-                            reviewAvgGt(searchRequestDto.getReviewFilter())
-                    )
-                    .orderBy(orderBySort(searchRequestDto.getSort()))
-                    .offset(pageable.getOffset())
-                    .limit(pageable.getPageSize())
-                    .fetch();
+            productIdList = getProductIdList_brandName(brandIdList, searchRequestDto, pageable);
+            totalCount = getTotalCount_brandName(brandIdList, searchRequestDto);
         }
 
-        if(!searchRequestDto.getKeyword().equals("ALL") && searchRequestDto.getSearchType().equals("브랜드명")) {
-            List<Long> brandIds = queryFactory.select(brand.brandId)
-                    .from(brand)
-                    .where(brand.name.contains(searchRequestDto.getKeyword()))
-                    .fetch();
-
-            countQuery = queryFactory.selectFrom(product)
-                    .innerJoin(parentCategory).on(product.category.parentCategory.eq(parentCategory.categoryId))
-                    .where(
-                            product.brand.brandId.in(brandIds),
-                            categoryEq(searchRequestDto.getCategoryFilter()),
-                            priceBetween(searchRequestDto.getMinPriceFilter(),searchRequestDto.getMaxPriceFilter()),
-                            reviewAvgGt(searchRequestDto.getReviewFilter())
-                    );
-
-            ids = queryFactory.select(product.productId)
-                    .from(product)
-                    .innerJoin(parentCategory).on(product.category.parentCategory.eq(parentCategory.categoryId))
-                    .where(
-                            product.brand.brandId.in(brandIds),
-                            categoryEq(searchRequestDto.getCategoryFilter()),
-                            priceBetween(searchRequestDto.getMinPriceFilter(),searchRequestDto.getMaxPriceFilter()),
-                            reviewAvgGt(searchRequestDto.getReviewFilter())
-                    )
-                    .orderBy(orderBySort(searchRequestDto.getSort()))
-                    .offset(pageable.getOffset())
-                    .limit(pageable.getPageSize())
-                    .fetch();
-        }
-
-        long totalCount = Objects.requireNonNull(countQuery).fetchCount();
-
-        if(ids.isEmpty()) {
+        if(productIdList.isEmpty()) {
             return new PageImpl<>(new ArrayList<>(), pageable, totalCount);
         }
 
-        List<SearchResponseDto> returnPost = queryFactory.select(Projections.fields(
-                SearchResponseDto.class,
-                product.productId,
-                product.name,
-                product.thumbnail,
-                brand.name.as("brandName"),
-                parentCategory.category,
-                product.price,
-                product.amount,
-                product.reviewNum,
-                product.reviewAvg
-        ))
-                .from(product)
-                .innerJoin(brand).on(product.brand.brandId.eq(brand.brandId))
-                .innerJoin(parentCategory).on(product.category.parentCategory.eq(parentCategory.categoryId))
-                .where(product.productId.in(ids))
-                .orderBy(orderBySort(searchRequestDto.getSort()))
-                .fetch();
+        List<SearchResponseDto> returnPost = queryFactory
+                                                .select(Projections.fields(
+                                                        SearchResponseDto.class,
+                                                        product.productId,
+                                                        product.name,
+                                                        product.thumbnail,
+                                                        brand.name.as("brandName"),
+                                                        parentCategory.category,
+                                                        product.price,
+                                                        product.amount,
+                                                        product.reviewNum,
+                                                        product.reviewAvg
+                                                    ))
+                                                .from(product)
+                                                .innerJoin(brand).on(product.brand.brandId.eq(brand.brandId))
+                                                .innerJoin(parentCategory).on(product.category.parentCategory.eq(parentCategory.categoryId))
+                                                .where(product.productId.in(productIdList))
+                                                .orderBy(orderBySort(searchRequestDto.getSort()))
+                                                .fetch();
 
         return new PageImpl<>(returnPost, pageable,totalCount);
+    }
+
+    private List<Long> getBrandIdList(SearchRequestDto searchRequestDto) {
+        return queryFactory
+                .select(brand.brandId)
+                .from(brand)
+                .where(brand.name.contains(searchRequestDto.getKeyword()))
+                .fetch();
+    }
+
+    private List<Long> getProductIdList_productName(SearchRequestDto searchRequestDto, Pageable pageable) {
+        return queryFactory
+                    .select(product.productId)
+                    .from(product)
+                    .innerJoin(parentCategory).on(product.category.parentCategory.eq(parentCategory.categoryId))
+                    .where(
+                            keywordContains(searchRequestDto.getKeyword()),
+                            categoryEq(searchRequestDto.getCategoryFilter()),
+                            priceBetween(searchRequestDto.getMinPriceFilter(),searchRequestDto.getMaxPriceFilter()),
+                            reviewAvgGt(searchRequestDto.getReviewFilter())
+                    )
+                    .orderBy(orderBySort(searchRequestDto.getSort()))
+                    .offset(pageable.getOffset())
+                    .limit(pageable.getPageSize())
+                    .fetch();
+    }
+
+    private List<Long> getProductIdList_brandName(List<Long> brandIdList, SearchRequestDto searchRequestDto, Pageable pageable) {
+        return queryFactory
+                    .select(product.productId)
+                    .from(product)
+                    .innerJoin(parentCategory).on(product.category.parentCategory.eq(parentCategory.categoryId))
+                    .where(
+                            product.brand.brandId.in(brandIdList),
+                            categoryEq(searchRequestDto.getCategoryFilter()),
+                            priceBetween(searchRequestDto.getMinPriceFilter(),searchRequestDto.getMaxPriceFilter()),
+                            reviewAvgGt(searchRequestDto.getReviewFilter())
+                    )
+                    .orderBy(orderBySort(searchRequestDto.getSort()))
+                    .offset(pageable.getOffset())
+                    .limit(pageable.getPageSize())
+                    .fetch();
+    }
+
+    private long getTotalCount_productName(SearchRequestDto searchRequestDto) {
+        JPQLQuery<Product> countQuery = queryFactory
+                                            .selectFrom(product)
+                                            .innerJoin(parentCategory).on(product.category.parentCategory.eq(parentCategory.categoryId))
+                                            .where(
+                                                    keywordContains(searchRequestDto.getKeyword()),
+                                                    categoryEq(searchRequestDto.getCategoryFilter()),
+                                                    priceBetween(searchRequestDto.getMinPriceFilter(),searchRequestDto.getMaxPriceFilter()),
+                                                    reviewAvgGt(searchRequestDto.getReviewFilter())
+                                            );
+
+        return countQuery.fetchCount();
+    }
+
+    private long getTotalCount_brandName(List<Long> brandIdList, SearchRequestDto searchRequestDto) {
+        JPQLQuery<Product> countQuery = queryFactory
+                                            .selectFrom(product)
+                                            .innerJoin(parentCategory).on(product.category.parentCategory.eq(parentCategory.categoryId))
+                                            .where(
+                                                    product.brand.brandId.in(brandIdList),
+                                                    categoryEq(searchRequestDto.getCategoryFilter()),
+                                                    priceBetween(searchRequestDto.getMinPriceFilter(),searchRequestDto.getMaxPriceFilter()),
+                                                    reviewAvgGt(searchRequestDto.getReviewFilter())
+                                            );
+
+        return countQuery.fetchCount();
+    }
+
+    private BooleanExpression keywordContains(String keyword) {
+        return keyword.equals("ALL") ? null : product.name.contains(keyword);
     }
 
     private BooleanExpression categoryEq(String inputCategory) {

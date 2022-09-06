04b0c1160348b1de2d207a82ad04e6c4e32ec2fd
ParkSoyun <soyp@naver.com>
2022-09-06 10:36:10 +0900

    Feat: 주문 취소 기능 추가구현 (#56)

---
+++ src/main/java/com/hanghae99/mocosa/layer/dto/order/OrderCancelResponseDto.java
@@ -0,0 +1,12 @@
+package com.hanghae99.mocosa.layer.dto.order;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderCancelResponseDto {
+    private String message;
+}
